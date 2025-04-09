a26662a746155fa36ff0b14ba7c7212ad57022fe
Hernandez13555 <168384717+Hernandez13555@users.noreply.github.com>
2025-04-09 06:39:10 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,6 +1,6 @@
 #Creacion de usuario
-create user 'edixon_adso2894667'@'localhost' identified by "Aprendiz2024";
+create user 'reinel_adso2894667'@'localhost' identified by "Aprendiz2025";
 #creando la base de datos de node.js
 create database node_adso2894667;
 #dandole privilegios
-grant all on node_adso2894667.* to edixon_adso2894667@localhost;
+grant all on node_adso2894667.* to reinel_adso2894667@localhost;
